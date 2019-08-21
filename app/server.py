41a80eea003f2c8a81152ef58c859730622f6272
peterveber <peterveber@gmail.com>
2019-08-21 20:42:07 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,8 +12,8 @@ from fastai.vision import *
 #model_file_name = 'model'
 #classes = ['black', 'grizzly', 'teddys']
 
-model_file_url = 'https://www.dropbox.com/s/1gcp2j9w8442bb4/stage-7_squeezenet.pth?raw=1'
-model_file_name = 'model6'
+model_file_url = 'https://www.dropbox.com/s/dzzzbb3oni6ebr1/stage-12_squeezenet?raw=1'
+model_file_name = 'model7'
 classes = ['9326871', '9332898', '9336923', '9338446', '9338454', '9338462', '9338489', '9338497', '9338519', '9338527', '9338535', '9338543', '9414649', '9416994', 'admars', 'ahodki', 'ajflem', 'ajones', 'ajsega', 'akatsi', 'ambarw', 'anpage', 'asamma', 'asewil', 'asheal', 'astefa', 'bplyce', 'cchris', 'ccjame', 'cferdo', 'cgboyc', 'cjcarr', 'cjdenn', 'cjsake', 'cmkirk', 'csanch', 'cshubb', 'cwchoi', 'dagran', 'dakram', 'dcbowe', 'dioann', 'djbirc', 'djhugh', 'djmart', 'dmwest', 'drbost', 'ekavaz', 'elduns', 'gdhatc', 'ggeorg', 'ggrego', 'gjhero', 'gjnorm', 'gmwate', 'gpapaz', 'gpsmit', 'gsreas', 'irdrew', 'jabins', 'jagrif', 'jcarte', 'jdbenm', 'jgloma', 'jlemon', 'jmedin', 'jrtobi', 'kaatki', 'kaknig', 'kdjone', 'khchan', 'khughe', 'kjwith', 'klclar', 'ksunth', 'lejnno', 'lfso', 'maasht', 'mberdo', 'mbutle', 'mdpove', 'mefait', 'mhwill', 'miaduc', 'mjhans', 'mpetti', 'muthay', 'nahaig', 'namull', 'ndbank', 'ndhagu', 'nhrams', 'njmoor', 'npbour', 'npmitc', 'nrclar', 'nrrbar', 'nwilli', 'ohpark', 'pacole', 'phughe', 'pmives', 'pshurr', 'pspliu', 'ptnich', 'rarobi', 'rgharr', 'rgspru', 'rjlabr', 'rlocke', 'rmcoll', 'rmpugh', 'rnpwil', 'rrowle', 'rsanti', 'saduah', 'saedwa', 'sbains', 'sidick', 'sjbeck', 'skumar', 'slbirc', 'smrobb', 'spletc', 'svkriz', 'swewin', 'swsmit', 'vpsavo', 'vstros', 'whussa', 'wjalbe', 'yfhsie']
 path = Path(__file__).parent
 
@@ -31,7 +31,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
     data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        ds_tfms=get_transforms(), size=180).normalize(imagenet_stats)
+        ds_tfms=get_transforms(), size=100).normalize(imagenet_stats)
     learn = cnn_learner(data_bunch, models.squeezenet1_1, pretrained=False)
     learn.load(model_file_name)
     return learn
@@ -58,7 +58,7 @@ async def analyze(request):
     st = str(predictions[0])
     nd = str(predictions[1])
     rd = str(predictions[2])
-    resultText = str("1st:" + st + "  2nd:" + nd + " 3rd:" + rd).replace('(','').replace(')','').replace(',','-')
+    resultText = str("1st:" + st + "  2nd:" + nd + " 3rdx:" + rd).replace('(','').replace(')','').replace(',','-')
     return JSONResponse({'result': str(resultText)})
     #return JSONResponse({'result': str(learn.predict(img)[0])})
     #_,_,losses = learn.predict(img)[0]
